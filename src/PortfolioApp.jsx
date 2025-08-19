16655b701f46e29238540a1b48180e54aa411896
pujhagr@gmail.com <pujhagr@gmail.com>
2025-08-19 23:15:43 +0530

    fix to code error

--- src/PortfolioApp.jsx
+++ src/PortfolioApp.jsx
@@ -497,7 +497,6 @@ export default function PortfolioApp() {
         ))}
       </Section>
     );
-  }
 
 
     const grouped = groups.map(g => ({ name: g.name, items: groupItems(g.test) })).filter(g => g.items.length > 0);
